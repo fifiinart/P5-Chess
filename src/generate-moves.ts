5d6aa9a8b7e9511276e573b2215a7fcfa9b2ae2a
Fifi Art <fifiinart@gmail.com>
2021-04-26 15:55:29 -0400

    Clean up code to move piece checking logic to *generateMoves*

--- src/generate-moves.ts
+++ src/generate-moves.ts
@@ -24,17 +24,27 @@ export enum KnightDirection {
 }
 
 export function generateMoves(position: number, piece: number, board: number[], boardState: BoardState): number[] {
-  return [
-    ...generateSlidingMoves(position, piece, board, boardState),
-    ...generatePawnMoves(position, piece, board, boardState),
-    ...generateKnightMoves(position, piece, board, boardState),
-    ...generateKingMoves(position, piece, board, boardState),
-  ].filter((v, i, a) => a.indexOf(v) === i); // filter out duplicates
+  let baseMoves: number[] = [];
+  switch (getPieceType(piece)) {
+    case Piece.Knight:
+      baseMoves.push(...generateKnightMoves(position, piece, board, boardState))
+      break;
+    case Piece.Pawn:
+      baseMoves.push(...generatePawnMoves(position, piece, board, boardState))
+      break;
+    case Piece.King:
+      baseMoves.push(...generateKingMoves(position, piece, board, boardState))
+      break;
+    case Piece.Bishop:
+    case Piece.Rook:
+    case Piece.Queen:
+      baseMoves.push(...generateSlidingMoves(position, piece, board, boardState))
+  }
+  baseMoves = [...new Set(baseMoves)] // filter out duplicates
+  return baseMoves
 }
 
-function generateKnightMoves(position: number, piece: number, board: number[], boardState: BoardState): number[] {
-
-  if (getPieceType(piece) !== Piece.Knight) return [];
+function generateKnightMoves(position: number, _piece: number, board: number[], boardState: BoardState): number[] {
 
   const positions: number[] = [];
 
@@ -51,37 +61,34 @@ function generateKnightMoves(position: number, piece: number, board: number[], b
 
 function generatePawnMoves(position: number, piece: number, board: number[], boardState: BoardState): number[] {
   const validMoves: number[] = [];
-  if (getPieceType(piece) === Piece.Pawn) {
-    const direction = getPieceColor(piece) === Piece.White ? Direction.U : Direction.D
-    const captureDirections = getPieceColor(piece) === Piece.White ? [Direction.UL, Direction.UR] : [Direction.DL, Direction.DR]
-    const haventMoved = getPieceColor(piece) === Piece.White ? boardState.whitePawnsThatHaventMoved : boardState.blackPawnsThatHaventMoved
+  const direction = getPieceColor(piece) === Piece.White ? Direction.U : Direction.D
+  const captureDirections = getPieceColor(piece) === Piece.White ? [Direction.UL, Direction.UR] : [Direction.DL, Direction.DR]
+  const haventMoved = getPieceColor(piece) === Piece.White ? boardState.whitePawnsThatHaventMoved : boardState.blackPawnsThatHaventMoved
 
-    // Test standard pawn move
-    const testPosition = position + direction;
+  // Test standard pawn move
+  const testPosition = position + direction;
+  if (board[testPosition] === Piece.None && slidingMoveContainedInBoard(position, testPosition)) validMoves.push(testPosition);
+
+  // Test capture ability
+  for (const dir of captureDirections) {
+    const testPosition = position + dir;
+    if (
+      pieceIsCapturable(testPosition, board, boardState) &&
+      slidingMoveContainedInBoard(position, testPosition) // the move doesnt go across a board edge
+    ) validMoves.push(testPosition);
+  }
+
+  // Test double pawn move
+  doublePawnMove: if (haventMoved.includes(position)) {
+    const testPosition = position + (direction * 2);
+    const inBetweenPosition = position + direction
+    if (board[inBetweenPosition] !== Piece.None) break doublePawnMove; // a pawn can't jump over a piece
     if (board[testPosition] === Piece.None && slidingMoveContainedInBoard(position, testPosition)) validMoves.push(testPosition);
-
-    // Test capture ability
-    for (const dir of captureDirections) {
-      const testPosition = position + dir;
-      if (
-        getPieceColor(board[testPosition]!) === getOppositeColor(boardState.colorToMove) && // we're actually capturing one of the opponent's pieces
-        getPieceType(board[testPosition]!) !== Piece.King && // it's not a king
-        slidingMoveContainedInBoard(position, testPosition) // the move doesnt go across a board edge
-      ) validMoves.push(testPosition);
-    }
-
-    // Test double pawn move
-    if (haventMoved.includes(position)) {
-      const testPosition = position + (direction * 2);
-      if (board[testPosition] === Piece.None && slidingMoveContainedInBoard(position, testPosition)) validMoves.push(testPosition);
-    }
   }
   return validMoves;
 }
 
-function generateKingMoves(position: number, piece: number, board: number[], boardState: BoardState): number[] {
-  if (getPieceType(piece) !== Piece.King) return [];
-
+function generateKingMoves(position: number, _piece: number, board: number[], boardState: BoardState): number[] {
   const positions: number[] = [];
 
   for (const direction of [...Object.values(Direction)].filter(x => typeof x === 'number') as Direction[]) {
@@ -147,4 +154,9 @@ function knightMoveContainedInBoard(testPosition: number, nextPosition: number):
   if (nextPosition >= BOARD_WIDTH * BOARD_HEIGHT) return false; // next position is below board
   if (Math.abs(testPosition % BOARD_WIDTH - nextPosition % BOARD_WIDTH) > 2) return false; // crossed horizontal edge
   return true
+}
+
+function pieceIsCapturable(testPosition: number, board: number[], boardState: BoardState) {
+  return getPieceColor(board[testPosition]!) === getOppositeColor(boardState.colorToMove) && // we're actually capturing one of the opponent's pieces
+    getPieceType(board[testPosition]!) !== Piece.King // it's not a king
 }
